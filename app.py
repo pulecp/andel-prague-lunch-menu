48eced132ec126de5e050fc1e4a8c5611d05c858
ReFuse <refuse@inuits.eu>
2020-01-28 11:53:27 +0100

    add tenants!

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
 # import datetime
 import restaurants
 
-restaurants = restaurants.run()
+pages = dict()
+
+DEFAULT_PAGE = 'andel'
+
+for page in os.listdir('pages'):
+    pages[page.replace('.json', '')] = restaurants.run(page)
 
 
-@app.route("/")
-def hello():
-    return render_template('index.html', restaurants=restaurants)
+@app.route('/', defaults={'name': DEFAULT_PAGE})
+@app.route('/<name>')
+def hello(name):
+    if (name not in pages):
+        name = DEFAULT_PAGE
+        # return render_template('404.html')
+    return render_template('index.html', restaurants=pages[name])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
